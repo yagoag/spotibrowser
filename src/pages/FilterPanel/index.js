01beba123ea2a6dc12f1aea321bd625bf2eb22d7
Yago Arroyo <yagoag@gmail.com>
2019-10-11 19:49:00 -0300

    Fixed useEffect functions with proper dependencies

--- src/pages/FilterPanel/index.js
+++ src/pages/FilterPanel/index.js
@@ -1,44 +1,47 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FilterElement from '../../components/FilterElement';
 import './style.css';
 
-const {REACT_APP_FILTERS_API_URL} = process.env;
+const { REACT_APP_FILTERS_API_URL } = process.env;
 
-const FilterPanel = ({filters, onChange}) => {
+const FilterPanel = ({ filters, onChange }) => {
   const [filterDefs, setFilterDefs] = useState([]);
-  const [time, setTime] = useState(null);
-  const prevTimeRef = useRef();
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(Date.now()), 30000);
     fetchFilterDefs();
-    prevTimeRef.current = time;
+
+    const interval = setInterval(() => {
+      fetchFilterDefs();
+    }, 30000);
 
     return () => clearInterval(interval);
-  });
+  }, []);
 
   const fetchFilterDefs = async () => {
-    if (filterDefs.length === 0 || time !== prevTimeRef.current) {
-      const {data} = await axios.get(REACT_APP_FILTERS_API_URL)
-      setFilterDefs(data.filters);
-    }
+    const { data } = await axios.get(REACT_APP_FILTERS_API_URL);
+    setFilterDefs(data.filters);
   };
 
-  const changeSingleFilter = (event) => {
-    const newState = {...filters};
+  const changeSingleFilter = event => {
+    const newState = { ...filters };
     newState[event.target.id] = event.target.value;
     onChange(newState);
-  }
-  
+  };
+
   return (
     <div className="filter-panel">
       <div className="app-title">SpotiBrowser</div>
       {filterDefs.map(def => (
-        <FilterElement key={def.id} definition={def} value={filters[def.id]} onChange={changeSingleFilter}></FilterElement>
+        <FilterElement
+          key={def.id}
+          definition={def}
+          value={filters[def.id]}
+          onChange={changeSingleFilter}
+        ></FilterElement>
       ))}
     </div>
   );
-}
+};
 
 export default FilterPanel;
--- src/pages/Playlists/index.js
+++ src/pages/Playlists/index.js
@@ -1,49 +1,48 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Playlist from '../../components/Playlist';
 import './style.css';
 
-const {REACT_APP_SPOTIFY_API_URL, REACT_APP_AUTH_API_URL} = process.env;
+const { REACT_APP_SPOTIFY_API_URL, REACT_APP_AUTH_API_URL } = process.env;
 
-const Playlists = ({filters}) => {
+const Playlists = ({ filters }) => {
   const [playlistMessage, setPlaylistMessage] = useState('');
   const [playlists, setPlaylists] = useState([]);
-  const [time, setTime] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const prevTimeRef = useRef();
-  const prevFiltersRef = useRef();
-  
+
   useEffect(() => {
-    const interval = setInterval(() => setTime(Date.now()), 30000);
-    fetchPlaylistData();
-    prevFiltersRef.current = filters;
-    prevTimeRef.current = time;
-
-    return () => clearInterval(interval);
-  });
-
-  const fetchPlaylistData = async () => {
-    if (playlists.length === 0 || filters !== prevFiltersRef.current || time !== prevTimeRef.current) {
+    const fetchPlaylistData = async () => {
       setIsLoading(true);
       const tokenResponse = await axios.get(REACT_APP_AUTH_API_URL);
       setIsLoading(true); // In case of concurrent fetches, at least will let user know it's fetching data after a while
-      const playlistResponse = await axios.get(REACT_APP_SPOTIFY_API_URL,
-        {
-          params: filters,
-          headers: {Authorization: 'Bearer ' + tokenResponse.data.access_token}
-        }
-      );
+      const playlistResponse = await axios.get(REACT_APP_SPOTIFY_API_URL, {
+        params: filters,
+        headers: { Authorization: 'Bearer ' + tokenResponse.data.access_token },
+      });
       setPlaylistMessage(playlistResponse.data.message);
       setPlaylists(playlistResponse.data.playlists.items);
       setIsLoading(false);
-    }
-  };
+    };
+
+    fetchPlaylistData();
+    const interval = setInterval(() => {
+      fetchPlaylistData();
+    }, 30000);
+
+    return () => clearInterval(interval);
+  }, [filters]);
 
   return (
     <div className="playlist-container">
-      {isLoading && <div className="loader-container"><div className="loader"></div></div>}
+      {isLoading && (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      )}
       <div className="title">{playlistMessage}</div>
-      {playlists.map((playlist, index) => <Playlist key={index} playlist={playlist} />)}
+      {playlists.map((playlist, index) => (
+        <Playlist key={index} playlist={playlist} />
+      ))}
     </div>
   );
 };
