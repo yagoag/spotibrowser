58b1552f815f3a930e9f2083aa86c27cdfc28e2a
Yago Arroyo <yagoag@gmail.com>
2019-10-24 22:58:46 -0300

    Updates

--- precache-manifest.7925624765bfb3c7d17598812f8ed845.js
+++ precache-manifest.7925624765bfb3c7d17598812f8ed845.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5629d44a313bc9f0c345f31e2f6d7c5f",
+    "revision": "6190e17e634c6fab2992a941df4adf73",
     "url": "/spotibrowser/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spotibrowser/static/css/2.ef8e8b31.chunk.css"
   },
   {
-    "revision": "c1e7ecf96dd6121a010f",
+    "revision": "6f44c6923538f9b5d87d",
     "url": "/spotibrowser/static/css/main.18d382c2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spotibrowser/static/js/2.7ecf87f0.chunk.js"
   },
   {
-    "revision": "c1e7ecf96dd6121a010f",
-    "url": "/spotibrowser/static/js/main.1167ba5c.chunk.js"
+    "revision": "6f44c6923538f9b5d87d",
+    "url": "/spotibrowser/static/js/main.04eccf10.chunk.js"
   },
   {
     "revision": "12a96782ae0c1903d65b",
