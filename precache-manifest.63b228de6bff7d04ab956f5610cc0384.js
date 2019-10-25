21456170ffde2dff4cd60f6302e9e635223dcf33
Yago Arroyo <yagoag@gmail.com>
2019-10-24 22:44:21 -0300

    Updates

---
+++ precache-manifest.63b228de6bff7d04ab956f5610cc0384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b1f3b1432950cbb250b8f4a7f8713d",
+    "url": "/spotibrowser/index.html"
+  },
+  {
+    "revision": "42747114e1219c4842da",
+    "url": "/spotibrowser/static/css/2.ef8e8b31.chunk.css"
+  },
+  {
+    "revision": "a0bdee066cc6e6c33b19",
+    "url": "/spotibrowser/static/css/main.18d382c2.chunk.css"
+  },
+  {
+    "revision": "42747114e1219c4842da",
+    "url": "/spotibrowser/static/js/2.7ecf87f0.chunk.js"
+  },
+  {
+    "revision": "a0bdee066cc6e6c33b19",
+    "url": "/spotibrowser/static/js/main.f9e7a006.chunk.js"
+  },
+  {
+    "revision": "12a96782ae0c1903d65b",
+    "url": "/spotibrowser/static/js/runtime~main.ab9fad04.js"
+  }
+]);
