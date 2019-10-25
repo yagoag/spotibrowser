075af0482697262ae54bec6a89402f4b7abcce90
Yago Arroyo <yagoag@gmail.com>
2019-10-24 22:40:05 -0300

    Added build folder for GitHub Pages

---
+++ docs/precache-manifest.fe24f8214a53dc9d62f2f3886532fbcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e519cec20a369cdf14717838f6d63bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42747114e1219c4842da",
+    "url": "/static/css/2.ef8e8b31.chunk.css"
+  },
+  {
+    "revision": "23c80e2e80dfaca65c04",
+    "url": "/static/css/main.18d382c2.chunk.css"
+  },
+  {
+    "revision": "42747114e1219c4842da",
+    "url": "/static/js/2.7ecf87f0.chunk.js"
+  },
+  {
+    "revision": "23c80e2e80dfaca65c04",
+    "url": "/static/js/main.64da257a.chunk.js"
+  },
+  {
+    "revision": "df77b16ebdb1db2342d2",
+    "url": "/static/js/runtime~main.5aaa2d27.js"
+  }
+]);
